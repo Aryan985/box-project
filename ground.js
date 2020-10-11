e35196515cf7a28542124a16dd3b3a141a72e7dc
Aryan985 <71887957+Aryan985@users.noreply.github.com>
2020-10-11 15:52:44 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,21 @@
+class Ground{
+
+    constructor(x,y,width,height){
+        var ground_property = {
+            isStatic: true
+          }
+          this.body = Bodies.rectangle(x,y,width,height,ground_property) 
+          this.width=width
+          this.height=height
+          World.add(world,this.body)
+
+    }
+
+    display(){
+
+        rectMode(CENTER)
+        rect(this.body.position.x,this.body.position.y,this.width,this.height) 
+
+    }
+
+}
